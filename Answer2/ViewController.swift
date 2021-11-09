47897bee1340277aea59aab8e4cb73c5b6b4f8a6
Sundar Aadityan <sundardevanathan2001@gmail.com>
2021-11-09 15:38:39 +0530

    Initial Commit

---
+++ Answer2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Answer2
+//
+//  Created by Sundara Aadithyan D on 09/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
